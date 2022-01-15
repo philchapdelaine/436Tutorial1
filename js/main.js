6bd29c666e8ef26125a7ee9e041cf9531fb9ce5c
philchapdelaine <pchapy@gmail.com>
2022-01-14 19:13:03 -0800

    got countries displaying

--- js/main.js
+++ js/main.js
@@ -1,35 +1,44 @@
-d3.select("body").append("p").text("Hello World!");
+d3.csv('data/cities_and_population.csv')
+.then(data => {
+    console.log('Data loading complete. Work with dataset.');
+    const filteredCities = data.filter(city => city.eu === "true");
+    const count = filteredCities.length;
 
-const sandwiches = [
-    { name: "Thesis", price: 7.95, size: "large" },
-    { name: "Dissertation", price: 8.95, size: "large" },
-    { name: "Highlander", price: 6.50, size: "small" },
-    { name: "Just Tuna", price: 6.50, size: "small" },
-    { name: "So-La", price: 7.95, size: "large" },
-    { name: "Special", price: 12.50, size: "small" }
-];
+    d3.select('body')
+	.append('p')
+	.text(`There are ${count} countries`);
 
-const svg = d3.select('body').append('svg')
-    .attr('width', 500)
-    .attr('height', 500);
+    filteredCities.forEach(city => {
+        city.population = +city.population;
+        city.x = +city.x;
+        city.y = +city.y;
+    });
 
-svg.selectAll('circle')
-    .data(sandwiches)
+    console.log(filteredCities);
+
+    const svg = d3.select('body').append('svg')
+    .attr('width', 750)
+    .attr('height', 550);
+
+    svg.selectAll('circle')
+    .data(filteredCities)
     .enter()
     .append('circle')
     .attr('r', d => {
-        if (d.size === 'small'){
-            return 5;
+        if (d.population < 1000000 ){
+            return 4;
         } else {
-            return 10;
+            return 8;
         }
     })
-    .attr('fill', d => {
-        if (d.price < 7) {
-            return 'red';
-        } else {
-            return 'blue';
-        }
-    })
-    .attr('cy', 20)
-    .attr('cx', (d, index) => (index * 50) + 10);
+    .attr('fill', 'blue')
+    .attr('stroke', 'black')
+    .attr('cx', d => d.x)
+    .attr('cy', d => d.y);
+
+})
+.catch(error => {
+    console.error('Error loading the data');
+});
+
+console.log('Do something else, without the data');
